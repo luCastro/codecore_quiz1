58c7c48369eb95e852baa521df72e3ef287c0911
Luciana Cunha <lucastro.marquise@gmail.com>
2018-06-25 10:54:22 -0700

    First view index.html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const knex = require("../db/client");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post("/", function(req,res){
+
 });
 
 module.exports = router;
